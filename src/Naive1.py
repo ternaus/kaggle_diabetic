c67683f3cc97e9c9aafe217e3318d99f782cd443
Vladimir Iglovikov <iglovikov@gmail.com>
2015-07-07 15:50:26 -0700

    bugfix in Naive. Now we get labels properly

--- src/Naive1.py
+++ src/Naive1.py
@@ -9,14 +9,43 @@ from nolearn.lasagne import NeuralNet
 from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import shuffle
 from lasagne.nonlinearities import softmax
+import matplotlib.image as mpimg
+import os
+import pandas as pd
+# print 'reading train'
+# fName = open('../data/train_green.pkl')
+# train = pickle.load(fName)
+# fName.close()
 
-print 'reading train'
-fName = open('../data/train_green.pkl')
-train = pickle.load(fName)
-fName.close()
+
+
+X = []
+
+fNames = os.listdir('../data/train')
+
+y = pd.DataFrame()
+y['image'] = fNames
+
+y = y.merge(pd.read_csv('../data/trainLabels.csv'))['level'].values
+
+
+#we are worinkg with impages with size 128x128 => new shape as for 1d array will be 16384
+#I am keeping only layer 0
+
+count = 0
+for fName in fNames:
+  img = mpimg.imread(os.path.join("..", "data", "train", fName))[:, :, 0]
+  # img = np.reshape(img, 16384)
+  X.append(img.tolist())
+  if count >= 1000 and count % 1000 == 0:
+    print count
+  count += 1
+
+
 
 print 'reading labels'
-train_ids = list(fName('../data/train_green_ids').read())
+# train_ids = list(fName('../data/train_green_ids').read())
+train_ids = y
 
 scaler = StandardScaler(with_mean=False)
 
@@ -24,7 +53,7 @@ encoder = LabelEncoder()
 y = encoder.fit_transform(train_ids.values).astype(np.int32)
 
 print 'scaling X'
-X = scaler.fit_transform(train.astype(np.float64)).astype(np.float32)
+X = scaler.fit_transform(X.astype(np.float64)).astype(np.float32)
 
 params = {
   'update_learning_rate': 0.01,
